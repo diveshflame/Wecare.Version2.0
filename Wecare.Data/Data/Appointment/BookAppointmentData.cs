8979d303e808415f633fdf1c263af56ae0efe0e2
Arjun <[empoliyan@gmail.com]>
2023-09-07 17:49:48 +0530

    Adding Book Appointment Controller and select query changes

--- Wecare.Data/Data/Appointment/BookAppointmentData.cs
+++ Wecare.Data/Data/Appointment/BookAppointmentData.cs
@@ -17,74 +17,93 @@ namespace WeCare.Data.Data
         public BookAppointmentData(ISqldataAccess _dbAccess)
         {
             dbAccess = _dbAccess;
- 
+
 
         }
-
-        List<DateTime> startTimes = new List<DateTime>();
-        List<DateTime> endTimes = new List<DateTime>();
-
         private readonly string getDepartmentQuery = "SELECT Department_Id from department";
-        public Task<IEnumerable<DepartmentModel>> BookGetCo() => dbAccess.LoadData<DepartmentModel, dynamic>(getDepartmentQuery, new { });
-        public async Task<AppointmentModel?> BookGetDoc(string SelectedDepartment)
+        public Task<IEnumerable<DepartmentModel>> GetDepartmentId() => dbAccess.LoadData<DepartmentModel, dynamic>(getDepartmentQuery, new { });
+        public async Task<AppointmentModel?> GetDoctorNames(string SelectedDepartment)
         {
-        
+
             string GetDoc = @"SELECT d.Doctor_name FROM doctor d 
                               JOIN department dp ON d.DEPARTMENT_ID = dp.Department_Id
-                              WHERE dp.Department_Name = @SelectedDepartment;";
-            var results2 = await dbAccess.LoadData<AppointmentModel, dynamic>(GetDoc, new { SelectedDepartment = SelectedDepartment });
-          
-            return results2.FirstOrDefault();
+                              WHERE dp.Department_Name = @SelectedDepartmentParameter;";
+            var result = await dbAccess.LoadData<AppointmentModel, dynamic>(GetDoc, new { SelectedDepartmentParameter = SelectedDepartment });
+
+            return result.FirstOrDefault();
 
         }
         //arjun
 
-        public async Task<AppointmentModel?> BookGetTime(string doc, DateTime selectedDate)
+        public async Task<AppointmentModel?> GetDoctorAvailableTime(string doc, DateTime selectedDate)
         {
 
-            string GetDocId = "SELECT Doctor_Id from doctor_table where @doctorName=Doctor_Name";
-            var results = await _db.LoadData<AppointmentModel, dynamic>(GetDocId, new { doctorName = doc });
-            string GetTime = "SELECT available_starttime, available_endtime FROM doctor_availability WHERE DATE(doctor_availability.available_starttime) = @time AND DATE(doctor_availability.available_endtime) = @time AND doctor_availability.doctor_id = @docid EXCEPT SELECT available_starttime, available_endtime FROM booking_table, doctor_availability WHERE booking_table.StartTime = available_starttime AND booking_table.doctor_id = @docid AND booking_table.EndTime = available_endtime AND Deleted_TimeStamp IS NULL ORDER BY available_starttime, available_endtime;";
-            var results2 = await _db.LoadData<AppointmentModel, dynamic>(GetTime, new { docid = results, time = selectedDate });
+            string GetTime = @"SELECT available_starttime, available_endtime
+                                FROM doctor_availability da
+                                WHERE 
+                                    DATE(da.available_starttime) = @time
+                                    AND DATE(da.available_endtime) = @time
+                                    AND da.doctor_id = (
+                                        SELECT Doctor_Id
+                                        FROM doctor_table
+                                        WHERE Doctor_Name = @doctorName
+                                    )
+                                EXCEPT
+                                SELECT available_starttime, available_endtime
+                                FROM booking_table bt
+                                JOIN doctor_availability da ON bt.APPOINTMENT_STARTTIME = da.available_starttime
+                                WHERE bt.doctor_id = (
+                                    SELECT Doctor_Id
+                                    FROM doctor_table
+                                    WHERE Doctor_Name = @doctorName
+                                )
+                                AND bt.Deleted_TimeStamp IS NULL
+                                ORDER BY available_starttime, available_endtime;
+                                ";
+            var results = await dbAccess.LoadData<AppointmentModel, dynamic>(GetTime, new { doctorName = doc, time = selectedDate });
+            return results.FirstOrDefault();
+        }
+        public async Task<AppointmentModel?> GetDepartmentID(string selectedDep, string doc)
+        {
+            string GetDepartmentID = @"SELECT department_id AS id
+                                        FROM department
+                                        WHERE @depSelected = consultant_desc
 
-            foreach (var appointment in results)
-            {
-                startTimes.Add(appointment.StartTime);
-                endTimes.Add(appointment.EndTime);
-            }
+                                        UNION
+
+                                        SELECT doctor_Id AS id
+                                        FROM doctor
+                                        WHERE @d = Doctor_Name;
+                                        ";
+
+
+            var results2 = await dbAccess.LoadData<AppointmentModel, dynamic>(GetDepartmentID, new { docSelected = doc, depSelected = selectedDep });//add userid
+
+            return results2.FirstOrDefault();
+
+        }
+        public async Task<AppointmentModel?> GetUserID()
+        {
+            string userID = "SELECT userid from userdetails where active_session = 1";
+            var results2 = await dbAccess.LoadData<AppointmentModel, dynamic>(userID, new { });//add userid
             return results2.FirstOrDefault();
         }
-        public async Task<AppointmentModel?> AddBook(string selectedDep, DateTime selectedDate, string doc, DateTime d1, DateTime d2)
+        public async Task InsertAppointment(string selectedDep, DateTime selectedDate, string doc, DateTime StartTime, DateTime EndTime)
         {
-            string GetConsultant = "SELECT consultant_id from consultant_Type where @depSelected = consultant_desc";
-            var results = await _db.LoadData<AppointmentModel, dynamic>(GetConsultant, new { depSelected = selectedDep });
-            string GetDoctor = "SELECT doctor_Id from doctor_table where @d=Doctor_Name";
-            var results2 = await _db.LoadData<AppointmentModel, dynamic>(GetDoctor, new { docSelected = doc });//add userid
-            string insertStatement = "INSERT INTO Booking_Table (userid,consultant_id,doctor_id,starttime,endtime) VALUES (@p,@consultType,@docSelected,@timeslot,@timeslot2)";
-            DateTime dc1 = DateTime.Now;
-            DateTime dc2 = DateTime.Now;
-            DateTime dc3 = DateTime.Now;
-            dc1 = d1;
-            dc2 = d2;
-            dc3 = d1.AddHours(1);
+            AppointmentModel? DeptID = await GetDepartmentID(selectedDep, doc);
+            AppointmentModel? UserID = await GetUserID();
+            string insertStatement = "INSERT INTO Booking_Table (userid,department_id,doctor_id,APPOINTMENT_STARTTIME,APPOINTMENT_ENDTIME) VALUES (@userID,@DeptType,@docSelected,@StartSlot,@EndSlot)";
+
             var parameters = new
             {
-                docSelected = doc
-
+                docSelected = doc,
+                userID = UserID,
+                DeptType = DeptID,
+                StartSlot = StartTime,
+                EndSlot = EndTime
             };
 
-            if (dc1 >= DateTime.Now && dc2 >= DateTime.Now)
-            {
-                while (dc1 != dc2)
-                {
-                    await _db.SaveData(insertStatement, parameters);
-                    dc1 = dc1.AddHours(1);
-                    dc3 = dc3.AddHours(1);
-                }
-            }
-
-            return results.FirstOrDefault();
-
+            await dbAccess.SaveData(insertStatement, parameters);
         }
     }
 }
--- Wecare.Data/Data/Doctor/AddDoctorData.cs
+++ Wecare.Data/Data/Doctor/AddDoctorData.cs
@@ -10,22 +10,25 @@ namespace WeCare.Data.Data.Doctor
 {
     public class AddDoctorData : IAddDoctorData
     {
-        private readonly ISqldataAccess _db;
-        public AddDoctorData(ISqldataAccess db)
+      
+        private readonly ISqldataAccess dbAccess;
+        public AddDoctorData(ISqldataAccess _dbAccess)
         {
-            _db = db;
+            dbAccess = _dbAccess;
+
+
         }
-        string GetCon = "SELECT consultant_desc AS Department_Name from consultant_type AS Department_Name";
+        string GetDepartmentNames = "SELECT consultant_desc AS Department_Name from consultant_type AS Department_Name";
     
-        public Task<IEnumerable<DepartmentModel>> GetDep() => _db.LoadData<DepartmentModel, dynamic>(GetCon, new { });
+        public Task<IEnumerable<DepartmentModel>> GetDep() => dbAccess.LoadData<DepartmentModel, dynamic>(GetCon, new { });
         public async Task AddDoctor(string text, string selectedConsultation)
         {
 
             string GetConID = "SELECT consultant_id AS Department_Id from consultant_Type where @Selectedconsult = consultant_desc";
-            var results = await _db.LoadData<DepartmentModel, dynamic>(GetConID, new { Selectedconsult = selectedConsultation });
+            var results = await dbAccess.LoadData<DepartmentModel, dynamic>(GetConID, new { Selectedconsult = selectedConsultation });
             int consultantId = results.FirstOrDefault()?.Department_Id ?? 0;
             string GetDocID = "SELECT doctor_Id from doctor_table where @Doctor_Name=doctor_name";
-            var results2 = await _db.LoadData<DoctorModel, dynamic>(GetDocID, new { Doctor_Name = text });
+            var results2 = await dbAccess.LoadData<DoctorModel, dynamic>(GetDocID, new { Doctor_Name = text });
 
             string InsertDoc = "Insert into doctor_table(Doctor_name,consultant_id) values(@DoctorN,@Consult_Id)";
             if (consultantId != null)
@@ -35,7 +38,7 @@ namespace WeCare.Data.Data.Doctor
             }
             else
             {
-                await _db.SaveData(InsertDoc, new { DoctorN = text, Consult_Id = consultantId });
+                await dbAccess.SaveData(InsertDoc, new { DoctorN = text, Consult_Id = consultantId });
             }
         }
 

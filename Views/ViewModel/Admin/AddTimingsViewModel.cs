1f2a9621c4c8f1e8c167f335de3d1af778fe3801
Arjun <[empoliyan@gmail.com]>
2023-09-12 14:05:00 +0530

    Views Changes

--- Views/ViewModel/Admin/AddTimingsViewModel.cs
+++ Views/ViewModel/Admin/AddTimingsViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using MVVM_App.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
 
 namespace Views.ViewModel.Admin
 {
    public class AddTimingsViewModel : ViewModelBase
     {
-    }
+        public ICommand SelectionChangedCommand { get; }
+        public AddTimingsViewModel() 
+        {
+
+            SelectionChangedCommand = new RelayCommand<EventArgs>(ExecuteSelectionChanged);
+        }
+        private void ExecuteSelectionChanged(EventArgs e)
+        {
+        }
+        }
 }
